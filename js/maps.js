1ad8a0c79a6b4052b593a9463830ba192afe2aa3
Rey Vega <reynaldovega2010@hotmail.com>
2022-10-19 17:11:29 -0500

    Adds geolocation functionality to google maps

--- js/maps.js
+++ js/maps.js
@@ -1,12 +1,47 @@
+let map;
+let marker;
+let watchID;
+let geoLoc;
+
+function showLocationOnMap(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+
+  const coord = { lat: latitude, lng: longitude };
+  marker.setPosition(coord);
+  map.setCenter(coord);
+}
+
+function getPosition() {
+  if (navigator.geolocation) {
+    var options = { timeout: 60000 };
+    geoLoc = navigator.geolocation;
+    watchID = geoLoc.watchPosition(showLocationOnMap, errorHandler, options);
+  } else {
+    alert("Your browser doesn't support geolocation");
+  }
+}
+
+function errorHandler(err) {
+  if (err.code == 1) {
+    alert("Error: negated access");
+  } else if (err.code == 2) {
+    alert("Error: position doesn't exist");
+  }
+}
+
 function initMap() {
-    var coord = {lat:-34.5956145 ,lng: -58.4431949};
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 10,
-      center: coord
-    });
-    var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
-    console.log("Connection Succesfully - Google Maps")
+  const coord = { lat: -34.5956145, lng: -58.4431949 };
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: coord,
+  });
+
+  marker = new google.maps.Marker({
+    position: coord,
+    map: map,
+  });
+
+  console.log("Connection Succesfully - Google Maps");
+  getPosition();
 }
